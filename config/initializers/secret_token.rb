1440e08dcfab731d6869be94f68ec1f58ec2f8a7
Duncan Sample <code@antimatters.co.uk>
2013-06-07 18:57:25 +0100

    Initial commit with base rails project and .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techradarcommunity::Application.config.secret_token = '0393821e8071bdf56bdc3a1d66a206000c8ccc014ecd206905689ab405e333ff6cfd6d52cfbf66da974a2d2c5641b8c2498dc6066c1300d1ca84d38c39950a6c'
