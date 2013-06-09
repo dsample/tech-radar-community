ddc9553c623818178099142f58ab57451755ba73
Duncan Sample <code@antimatters.co.uk>
2013-06-09 20:40:42 +0100

    Added commentable to Products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,10 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
 
+    @commentable = @product
+    @comments = @commentable.comments
+    @comment = Comment.new
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @product }
