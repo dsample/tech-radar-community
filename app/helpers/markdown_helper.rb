c1b94f40048950b35282206d06d3b1a7d165f853
Duncan Sample <code@antimatters.co.uk>
2013-07-04 22:34:44 +0100

    Added Markdown helper

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -7,4 +7,8 @@ module MarkdownHelper
 			throw "MarkDown failed to load"
 		end
 	end
+
+	def md_help
+		link_to image_tag('md.png', target: "_blank", alt: "Markdown formatted", title: "Markdown formatted, click to open help",  "data-tooltip" => "", class: ["has-tip", "tip-right"]), '#', "data-reveal-id" => "markdownHelp"
+	end
 end
