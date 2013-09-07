8634fecc9bf6988b6f6c2b8ca510aa5c2b64ddd2
Duncan Sample <code@antimatters.co.uk>
2013-09-07 16:11:45 +0100

    Renamed radar welcome view to newradar, in preparation for a logged-out view

--- app/controllers/radar_controller.rb
+++ app/controllers/radar_controller.rb
@@ -1,6 +1,6 @@
 class RadarController < ApplicationController
   def index
   	@categories = Category.includes(:technologies).where(:technologies => {:deleted_at => nil}).select{ |x| x.technologies.count > 0 }
-  	render :welcome if @categories.empty?
+  	render :newradar if @categories.empty?
   end
 end
