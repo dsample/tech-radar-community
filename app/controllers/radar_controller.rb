ab121aefe8664f102920348ff48e0be9216e97a1
Duncan Sample <code@antimatters.co.uk>
2013-09-08 16:22:40 +0100

    Added Welcome page

--- app/controllers/radar_controller.rb
+++ app/controllers/radar_controller.rb
@@ -1,5 +1,6 @@
 class RadarController < ApplicationController
   def index
+  	render :welcome, layout: 'fullwidth' unless logged_in?
   	@categories = Category.includes(:technologies).where(:technologies => {:deleted_at => nil}).select{ |x| x.technologies.count > 0 }
   	render :newradar if @categories.empty?
   end
