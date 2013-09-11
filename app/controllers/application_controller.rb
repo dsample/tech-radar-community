f80c3b1787756c5011da27f0a30e8e4e0b6e6a54
Duncan Sample <code@antimatters.co.uk>
2013-09-11 19:49:06 +0100

    Cleaned up leftover oauth provider lookup enum

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
   helper_method :current_user
   helper_method :logged_in?
   helper_method :authenticate_user!
-  helper_method :provider_name
-  
+
   private
   
 	def current_user
@@ -15,24 +15,7 @@ class ApplicationController < ActionController::Base
 	def logged_in?
 		current_user.present?
 	end
-	
-	def provider_name(provider)
-		case provider
-			when "google_oauth2"
-				"Google"
-			when "twitter"
-				"Twitter"
-			when "facebook"
-				"Facebook"
-			when "linkedin"
-				"LinkedIn"
-			when "developer"
-				"Developer"
-			else
-				"Unknown"
-		end
-	end
-	
+		
 	def authenticate_user!
     if !current_user
       redirect_to login_url
